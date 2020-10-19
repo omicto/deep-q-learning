e5a60f54f4bee750b51a27d5fc7abea152e0eb1b
omicto <tevas.dlr@gmail.com>
2020-10-18 23:22:47 -0600

    Parameterize agent

--- dqn.py
+++ dqn.py
@@ -7,17 +7,17 @@ from keras.models import Sequential
 from keras.layers import Dense
 from keras.optimizers import Adam
 
-EPISODES = 1000
+EPISODES = 5
 
 class DQNAgent:
-    def __init__(self, state_size, action_size):
+    def __init__(self, state_size, action_size, **kwargs):
         self.state_size = state_size
         self.action_size = action_size
         self.memory = deque(maxlen=2000)
-        self.gamma = 0.95    # discount rate
-        self.epsilon = 1.0  # exploration rate
-        self.epsilon_min = 0.01
-        self.epsilon_decay = 0.995
+        self.gamma = kwargs.get("gamma", 0.95)
+        self.epsilon = kwargs.get("epsilon", 1.0)
+        self.epsilon_min = kwargs.get("epsilon_min", 0.01)
+        self.epsilon_decay = kwargs.get("epsilon_decay", 0.995)
         self.learning_rate = 0.001
         self.model = self._build_model()
 
@@ -45,8 +45,7 @@ class DQNAgent:
         for state, action, reward, next_state, done in minibatch:
             target = reward
             if not done:
-                target = (reward + self.gamma *
-                          np.amax(self.model.predict(next_state)[0]))
+                target = (reward + self.gamma * np.amax(self.model.predict(next_state)[0]))
             target_f = self.model.predict(state)
             target_f[0][action] = target
             self.model.fit(state, target_f, epochs=1, verbose=0)
@@ -60,19 +59,11 @@ class DQNAgent:
         self.model.save_weights(name)
 
 
-if __name__ == "__main__":
-    env = gym.make('CartPole-v1')
-    state_size = env.observation_space.shape[0]
-    action_size = env.action_space.n
-    agent = DQNAgent(state_size, action_size)
-    # agent.load("./save/cartpole-dqn.h5")
-    done = False
-    batch_size = 32
-
-    for e in range(EPISODES):
+def train_agent(agent, state_sieze, action_size, episodes, max_t, batch_size):
+    for e in range(episodes):
         state = env.reset()
         state = np.reshape(state, [1, state_size])
-        for time in range(500):
+        for time in range(max_t):
             # env.render()
             action = agent.act(state)
             next_state, reward, done, _ = env.step(action)
@@ -82,9 +73,17 @@ if __name__ == "__main__":
             state = next_state
             if done:
                 print("episode: {}/{}, score: {}, e: {:.2}"
-                      .format(e, EPISODES, time, agent.epsilon))
+                      .format(e, episodes, time, agent.epsilon))
                 break
             if len(agent.memory) > batch_size:
                 agent.replay(batch_size)
-        # if e % 10 == 0:
-        #     agent.save("./save/cartpole-dqn.h5")
+
+if __name__ == "__main__":
+    env = gym.make('CartPole-v1')
+    state_size = env.observation_space.shape[0]
+    action_size = env.action_space.n
+    agent = DQNAgent(state_size, action_size)
+    done = False
+    batch_size = 10
+
+    train_agent(agent, state_size, action_size, 100, 500, batch_size)
