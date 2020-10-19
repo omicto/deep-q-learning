5cb8e5252f479682d75744617f58775c218c49bc
omicto <tevas.dlr@gmail.com>
2020-10-18 23:39:11 -0600

    package2

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="DQN", # Replace with your own username
+    name="deeprl", # Replace with your own username
     version="0.0.1",
     author="Example Author",
     author_email="author@example.com",
